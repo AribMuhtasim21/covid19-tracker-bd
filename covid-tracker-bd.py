50b757bd48d7f91a19f58de417e3a85bfbc42a0a
AribMuhtasim21 <69956399+AribMuhtasim21@users.noreply.github.com>
2021-01-11 16:23:09 +0600

    first commit

--- covid-tracker-bd.py
+++ covid-tracker-bd.py
@@ -12,7 +12,7 @@ f_name = date_ + " " + month_  + " " + year_ + ".txt"
 URL = 'https://corona.gov.bd/lang/en'
 headers = {
     "User-Agent":
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko Chrome/87.0.4280.66 Safari/537.36'
+    'YOUR_USER_AGENT_HERE'
 }
 
 page = requests.get(URL, headers=headers)
